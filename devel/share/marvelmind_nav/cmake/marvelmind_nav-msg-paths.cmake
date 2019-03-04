35bdca847bc35f8a93a41aadf1a4659f5f4640a6
Jason <janderson271@berkeley.edu>
2019-03-04 15:54:46 -0800

    Functioning marvelmind

---
+++ devel/share/marvelmind_nav/cmake/marvelmind_nav-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(marvelmind_nav_MSG_INCLUDE_DIRS "/home/jasonanderson/ME102B_Project/ros_workspace/src/marvelmind_nav/msg")
+set(marvelmind_nav_MSG_DEPENDENCIES std_msgs)
