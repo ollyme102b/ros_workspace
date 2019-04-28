fixed rotation issue

src/olly_control/src/OllyController.py
@@ -51,7 +51,7 @@ class OllyController(object):
         :param message: of ROS message type Pose
         """
         orientation = message.pose.orientation
-        quaternion = [orientation.w, orientation.x, orientation.y, orientation.z]
+        quaternion = [orientation.x, orientation.y, orientation.z, orientation.w]
         yaw = euler_from_quaternion(quaternion)[2]
         self._position = np.array([message.pose.position.x, message.pose.position.y, yaw])
 

src/olly_control/src/OllyPositionMPCController.py
@@ -32,7 +32,7 @@ class OllyPositionMPCController(OllyController):
         position set point ROS call back function
         :param message: of ROS message type Pose
         """
-        self._position_set_point = np.array([message.position.x, message.position.y, 0])  ### only actuates 0 yaw
+        self._position_set_point = np.array([message.position.x, message.position.y, message.orientation.z])  ### only actuates 0 yaw
 
     def _compute_control_action(self):
         """