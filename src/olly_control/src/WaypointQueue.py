f71f7b2f76c2d07512d2ccbaf874199422749bac
jasonanderson <janderson271@berkeley.edu>
2019-04-29 01:46:02 -0700

    added orientation info to waypoint

--- src/olly_control/src/WaypointQueue.py
+++ src/olly_control/src/WaypointQueue.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-import numpy as np 
+import numpy as np
 import rospy
 from geometry_msgs.msg import Pose, PoseStamped
 import sys
-sys.path.append(sys.path[0].replace('/olly_control/src',''))
+
+sys.path.append(sys.path[0].replace('/olly_control/src', ''))
 from LaunchHelper import get_and_set_params
 
 
@@ -27,7 +28,7 @@ class WaypointQueue:
         self._olly_setpoint_publisher = rospy.Publisher('/' + self._olly_name + "/position_setpoint", Pose,
                                                         queue_size=1)
 
-        self._olly_position = PoseStamped()
+        self._olly_position = Pose()
         self._olly_position_subscriber = self._position_subscriber = rospy.Subscriber(
             '/' + self._olly_name + "/position", PoseStamped, self._position_callback)
 
@@ -35,31 +36,23 @@ class WaypointQueue:
             '/' + self._olly_name + "/waypoint_queue", Pose, self._waypoint_queue_callback)
 
     def _position_callback(self, message):
-        self._olly_position = message
+        self._olly_position = message.pose
 
     def _waypoint_queue_callback(self, message):
-        self._append_waypoint(message.position.x, message.position.y)
+        self.queue.append(message)
 
     def _publish_waypoint(self):
         self._olly_setpoint_publisher.publish(self._olly_setpoint)
 
-    def _set_setpoint(self, x, y):
-        self._olly_setpoint.position.x = x
-        self._olly_setpoint.position.y = y
-
     def _publish_next_waypoint(self):
-        next = self.queue.pop(0)
-        self._set_setpoint(next[0], next[1])
+        self._olly_setpoint = self.queue.pop(0)
         self._publish_waypoint()
-
-    def _append_waypoint(self, x, y):
-        self.queue.append(np.array([x, y]))
 
     def _assert_control(self):
         if len(self.queue) == 0:
             return
-        distance = (self._olly_position.pose.position.x - self._olly_setpoint.position.x) ** 2 + (
-                self._olly_position.pose.position.y - self._olly_setpoint.position.y) ** 2
+        distance =   (self._olly_position.position.x - self._olly_setpoint.position.x) ** 2 \
+                   + (self._olly_position.position.y - self._olly_setpoint.position.y) ** 2
         if distance < self._tolerance_radius ** 2:
             self._publish_next_waypoint()
 
